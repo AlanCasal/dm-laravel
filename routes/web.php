57f72e9d7eb3ac154e878cf65ce5c6b37878bdd6
Alan Casal <foxy.adc@gmail.com>
2019-02-03 00:28:37 -0300

    pequeños avances

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+	return view('index');
 });
+
+// Route::get('/user/{id}', function($id) {
+// 	return "Mostrando detalle de user {$id}";
+// })->where('id', '[0-9]+'); // EXPRESIÓN REGULTAR: el parametro que recibe tiene que ser un numero
+//
+// Route::get('/user/nuevo', function() { // el parametro que recibe tiene que ser el string 'nuevo'
+// 	return 'Crear user nuevo';
+// });
+//
+// Route::get('/saludo/{name}/{nick?}', function($name, $nick = null) {
+// 	if ($nick): return "Bienvenido {$name}, tu nick es {$nick}";
+// 	else:		return "Bienvenido {$name}, aún no tienes un nick";
+// 	endif;
+// });
