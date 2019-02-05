5e630843911ceab0ff78b71b7fbd56b29f6a3bf3
Alan Casal <foxy.adc@gmail.com>
2019-02-04 22:40:35 -0300

    navbar terminada

--- resources/views/components/products1.blade.php
+++ resources/views/components/products1.blade.php
@@ -1,58 +1,26 @@
-{{-- Cards grandes --}}
-<div class="card text-center col-sm-12 col-md-6 col-lg-3 shadow-sm p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
-	<a href=""><img class="card-img-top" src='{{ asset("img/1.jpg") }}' alt=""></a>
+{{-- 3 Cards grandes - ofertas PCs armadas --}}
+@for ($i=0; $i < 3; $i++)
+	@php
+	$pcRandom     = rand(49, 54);
+	$precioRandom = rand(2000, 8000) * 5;
+	@endphp
 
-	<div class="card-body hover"></div>
+	<div class="card text-center col-sm-12 col-md-6 col-lg-3 shadow-sm p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
+		<a href=""><img class="card-img-top" src='{{ asset("img/{$pcRandom}.jpg") }}' alt=""></a>
 
-	<div class="card-footer bg-transparent">
-		<li> descripcion sarlanga</li>
-		<h4 class="card-title">$ 9999</h4>
-		<label><strong>Agregar al Carrito</strong></label>
-		<div class="d-flex justify-content-center">
-			<input class="form-control col-4" type="number" name="quantity" value="1" min='1' max='10'>
+		<div class="card-body hover"></div>
 
-			<button class='btn btn-info add-to-cart' type="submit">
-				<i class="fas fa-cart-plus" style="font-size: 1em"></i>
-			</button>
+		<div class="card-footer bg-transparent">
+			<li> descripcion sarlanga</li>
+			<h4 class="card-title">$ {{$precioRandom}},00 ARS</h4>
+			<label><strong>Agregar al Carrito</strong></label>
+			<div class="d-flex justify-content-center">
+				<input class="form-control col-4" type="number" name="quantity" value="1" min='1' max='10'>
+
+				<button class='btn btn-info add-to-cart' type="submit">
+					<i class="fas fa-cart-plus" style="font-size: 1em"></i>
+				</button>
+			</div>
 		</div>
 	</div>
-</div>
-
-<div class="card text-center col-sm-12 col-md-6 col-lg-3 shadow-sm p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
-	<a href=""><img class="card-img-top" src='{{ asset("img/2.jpg") }}' alt=""></a>
-
-	<div class="card-body hover"></div>
-
-	<div class="card-footer bg-transparent">
-		<li> descripcion sarlanga</li>
-		<h4 class="card-title">$ 9999</h4>
-		<label><strong>Agregar al Carrito</strong></label>
-		<div class="d-flex justify-content-center">
-			<input class="form-control col-4" type="number" name="quantity" value="1" min='1' max='10'>
-
-			<button class='btn btn-info add-to-cart' type="submit">
-				<i class="fas fa-cart-plus" style="font-size: 1em"></i>
-			</button>
-		</div>
-	</div>
-</div>
-
-<div class="card text-center col-sm-12 col-md-6 col-lg-3 shadow-sm p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
-	<a href=""><img class="card-img-top" src='{{ asset("img/3.jpg") }}' alt=""></a>
-
-	<div class="card-body hover"></div>
-
-	<div class="card-footer bg-transparent">
-		<li> descripcion sarlanga</li>
-		<h4 class="card-title">$ 9999</h4>
-		<label><strong>Agregar al Carrito</strong></label>
-		<div class="d-flex justify-content-center">
-			<input class="form-control col-4" type="number" name="quantity" value="1" min='1' max='10'>
-
-			<button class='btn btn-info add-to-cart' type="submit">
-				<i class="fas fa-cart-plus" style="font-size: 1em"></i>
-			</button>
-		</div>
-	</div>
-</div>
-</div> {{-- Cierro el ROW de cards --}}
+@endfor
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'HOME - Dragon Market - Equipos y Componentes para Gamers')
 
 @section('section')
-	@include('components/carousel')
+	@include('components/carousel1')
 
 	<div class="container">
 		<div class="row">
 			@include('components/categories')
 
 			@include('components/products1')
+			@include('components/products2')
 
 		</div> {{-- row --}}
 	</div> {{-- container --}}
