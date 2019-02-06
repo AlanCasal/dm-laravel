9708e2522dea6ae50371a1f81e79231a1c3fdd23
Alan Casal <foxy.adc@gmail.com>
2019-02-05 22:17:15 -0300

    navbar retoques

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-expand-md navbar-light fixed-top" id="navbar-edit">
-=======
-{{-- https://stackoverflow.com/questions/42586729/bootstrap-4-change-hamburger-toggler-color --}}
-<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top" id="navbar-edit">
->>>>>>> e661149d45e839e71274a7000401731b30eb9166
+<nav class="navbar navbar-expand-md navbar-dark fixed-top" id="navbar-edit">
 	<div class="container">
 
 		{{-- Logo --}}
 		<a class="navbar-brand" href="#">
 			<img src='{{asset("img/DMHead.png")}}' alt='Logo' style='width: 50px;'>
-			<img src='{{asset("img/DMText.png")}}' alt='Logo' style='width: 200px;'>
+			<img class="d-none d-sm-inline-block" src='{{asset("img/DMText.png")}}' alt='Logo' style='width: 200px;'>
 		</a>
 
 		{{-- Boton toggler responsive --}}
-		<button class="navbar-toggler navbar-default" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
@@ -21,29 +16,31 @@
 		<div class="collapse navbar-collapse" id="navbarMenu">
 
 			{{-- Search box --}}
-			<div class="row no-gutters ml-auto col-lg-6">
-				<div class="col">
-					<input class="form-control form-control-no-border border-info border-right-0 rounded-0" type="search" value="" placeholder="Buscar productos...">
-				</div>
-				<div class="col-auto">
-					<button class="btn btn-outline-info border-left-0 rounded-0 rounded-right search-btn noBoxShadow-btn" type="button">
-						<i class="fa fa-search"></i>
-					</button>
+			<div class="ml-auto d-none d-lg-inline-block col-lg-6">
+				<div class="row no-gutters">
+					<div class="col">
+						<input class="form-control form-control-no-border border-info border-right-0 rounded-0" type="search" value="" placeholder="Buscar productos...">
+					</div>
+					<div class="col-auto">
+						<button class="btn btn-outline-info border-left-0 rounded-0 rounded-right search-btn noBoxShadow-btn" type="button">
+							<i class="fa fa-search"></i>
+						</button>
+					</div>
 				</div>
 			</div>
 
 			{{-- Botones --}}
-			<ul class="navbar-nav ml-auto">
+			<ul class="navbar-nav ml-auto flex-row">
 				<li class="nav-item">
 					<a href="#" class="nav-link"><i class="fas fa-shopping-cart"></i> (0)</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item ml-auto">
 					<a href="#" class="nav-link"><i class="fas fa-sign-in-alt"></i> Ingresar</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item ml-auto">
 					<a href="#" class="nav-link"><i class="fas fa-user-edit"></i> Registrarse</a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item ml-auto">
 					<a href="#" class="nav-link"><i class="fas fa-question-circle"></i> Ayuda</a>
 				</li>
 			</ul>
