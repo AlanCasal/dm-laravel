326d1ef4063cdb3cfc17f84a9ed9093ba05865e1
Alan Casal <foxy.adc@gmail.com>
2019-02-12 17:21:09 -0300

    users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,26 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
-	/**
-	* Run the migrations.
-	*
-	* @return void
-	*/
-	public function up()
-	{
-		Schema::create('users', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->timestamp('email_verified_at')->nullable();
-			$table->string('password');
-			$table->rememberToken();
-			$table->timestamps();
-		});
-	}
+class CreateUsersTable extends Migration {
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->increments('id');
+      $table->timestamps();
+      $table->rememberToken();
+      $table->string('first_name');
+      $table->string('last_name');
+      $table->string('email')->unique();
+      $table->string('password');
+      $table->string('avatar')->default('public/users/default.jpg');
+      $table->integer('active')->default(1);
 
-	/**
-	* Reverse the migrations.
-	*
-	* @return void
-	*/
-	public function down()
-	{
-		Schema::dropIfExists('users');
-	}
+      // $table->integer('cart_id')->unsigned(); // en la clave foranea tiene que ir un unsigned para que no se rompa
+      // $table->foreign('cart_id')->references('id')->on('cart')->onDelete('cascade'); // establezco la relacion de la clave foreanea.
+    });
+  }
+
+  public function down() {
+    Schema::dropIfExists('users');
+  }
 }
